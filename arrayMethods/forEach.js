bbe52010db738839c7ae8e40fc78c51d64b664db
Jamshadj <jamshadalij2004@gmail.com>
2023-06-23 22:03:21 +0530

    some examples

--- arrayMethods/forEach.js
+++ arrayMethods/forEach.js
@@ -1,84 +1,104 @@
-// forEach() that allows you to iterate over each element of the array and perform a specific action on it.
+// // forEach() that allows you to iterate over each element of the array and perform a specific action on it.
 
-// The forEach() method is called on the array object. It takes a callback function as an argument, which is executed for each element of the array. 
+// // The forEach() method is called on the array object. It takes a callback function as an argument, which is executed for each element of the array. 
 
-// The callback function receives the current element as its parameter, and you can perform any desired action within the function.
+// // The callback function receives the current element as its parameter, and you can perform any desired action within the function.
 
-//In JavaScript, the forEach() method accepts a callback function with up to three parameters: the current element, the index of the current element, and the array being traversed.
+// //In JavaScript, the forEach() method accepts a callback function with up to three parameters: the current element, the index of the current element, and the array being traversed.
 
-//Example One
+// //Example One
 
-const a=[1,2,3,4,6]
+// const a=[1,2,3,4,6]
 
-a.forEach(callback)
+// a.forEach(callback)
 
-function callback(element,index,array) {
-    console.log(element)
-}
-//using arrow function
+// function callback(element,index,array) {
+//     console.log(element)
+// }
+// //using arrow function
 
-a.forEach(element => {
-    console.log(element)
-});
-//output of the two methods are same
-/*
-1
-2
-3
-4
-6
-*/
+// a.forEach(element => {
+//     console.log(element)
+// });
+// //output of the two methods are same
+// /*
+// 1
+// 2
+// 3
+// 4
+// 6
+// */
 
-a.forEach(callback)
+// a.forEach(callback)
 
-function callback(element,index,array) {
-    console.log(`a[${index}]=${element}`)
-}
+// function callback(element,index,array) {
+//     console.log(`a[${index}]=${element}`)
+// }
 
-//output
+// //output
 
-/*
-a[0]=1
-a[1]=2
-a[2]=3
-a[3]=4
-a[4]=6
-*/
+// /*
+// a[0]=1
+// a[1]=2
+// a[2]=3
+// a[3]=4
+// a[4]=6
+// */
 
-//to find the sum of numbers in the array 
+// //to find the sum of numbers in the array 
 
-let sum=0;
-a.forEach(element=>{
-    sum+=element})
-console.log(sum);
+// let sum=0;
+// a.forEach(element=>{
+//     sum+=element})
+// console.log(sum);
 
-//out put
-//sum=16
+// //out put
+// //sum=16
 
-//example two how many times letters reapeat in an array
+// //example two how many times letters reapeat in an array
 
-const letters=['a','b','d','a','d','c']
+// const letters=['a','b','d','a','d','c']
 
-const count={}
+// const count={}
 
-letters.forEach((element)=>{
-    if (count[element]) {
-        count[element]++
-    }else{
-        count[element]=1
-    }
-})
-console.log(count)
+// letters.forEach((element)=>{
+//     if (count[element]) {
+//         count[element]++
+//     }else{
+//         count[element]=1
+//     }
+// })
+// console.log(count)
 // output
 // {a: 2, b: 1, d: 2, c: 1}
 
-//using forEach and reduce method 
-// find total mark of each and every one
-const students=[{ 
-    name:"john",
-    mark:[12,32,12,13,15]
-},{
-    name:"mark",
-    mark:[23,12,21,32,21]
-}]
-       
+//1 Print the square of each number in an array.
+
+const number=[1,2,3,4,5]
+let squareOfEachNumber={}
+number.forEach((num)=>{
+    const sqnum=num*num;
+  squareOfEachNumber[`square[${num}]`]=sqnum
+})
+console.log(squareOfEachNumber);
+
+//output
+// {
+//     'square[1]': 1,
+//     'square[2]': 4,
+//     'square[3]': 9,
+//     'square[4]': 16,
+//     'square[5]': 25
+//   }
+
+//Find and print the maximum value in an array.
+
+const maximumValue=[1,2,3,4,5,32,43,4232,32,323]
+let maximum=null;
+maximumValue.forEach((value)=>{
+  if (value>maximum) {
+    maximum=value
+  }
+})
+console.log(maximum);
+
